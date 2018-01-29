0e13f4ef9142903be6305474209325e6dfaca5eb
daboland <damianvboland@gmail.com>
2018-01-29 14:34:03 +0000

    fixing passlib error

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from passlib.apps import custom_app_context as pwd_context
+#from passlib.apps import custom_app_context as pwd_context
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -27,5 +27,3 @@ class User(db.Model):
         except NameError:
             return str(self.id)  # python 3
 
-    def hask_password(self, password):
-        return pwd_context.verify(password, self.hashed_password)
